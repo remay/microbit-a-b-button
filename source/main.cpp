2899c5150ae0061cab2aa010d7979b7d9917e890
Robert May <rob@themayfamily.me.uk>
2015-11-24 11:48:41 +0100

    Correct images

--- source/main.cpp
+++ source/main.cpp
@@ -15,20 +15,60 @@
 
 #include "MicroBit.h"
 
-void onButtonAClick(MicroBitEvent evt) {   
-    uBit.display.print('A');  
+const MicroBitImage play_img ("0,1,0,0,0\n0,1,1,0,0\n0,1,1,1,0\n0,1,1,0,0\n0,1,0,0,0\n");
+const MicroBitImage pause_img("0,0,0,0,0\n0,1,0,1,0\n0,1,0,1,0\n0,1,0,1,0\n0,0,0,0,0\n");
+
+#define SCREEN_STATE_UNKNOWN 0
+#define SCREEN_LOGO_UP       1
+#define SCREEN_LOGO_DOWN     2
+#define SCREEN_SCREEN_UP     3
+int screen_state = SCREEN_STATE_UNKNOWN;
+
+void onPlayPauseEvent(MicroBitEvent evt) {   
+    if(evt.value == 0x0001 ) {
+	uBit.display.print(play_img,0,0,0,0);
+    }
+    else if(evt.value == 0x0002 ) {
+	uBit.display.print(pause_img,0,0,0,0);
+    }
+    else {
+        uBit.display.print('?');
+    }
 }
 
-void onButtonBClick(MicroBitEvent evt) {   
-    uBit.display.print('B');  
+void detect_screen() {
+    // Detects if the logo up/down orientation has changed and raises events.
+    int  accx        = uBit.accelerometer.getX();
+    int  accy        = uBit.accelerometer.getY();
+    int  accz        = uBit.accelerometer.getZ();
+    bool is_vertical = (abs(accx) + abs(accz)) < 500;
+    bool is_flat     = (abs(accx) + abs(accy)) < 500;
+
+    if      ((accy >  500) && is_vertical && (screen_state != SCREEN_LOGO_UP)){
+        screen_state = SCREEN_LOGO_UP;
+        // send logo up event
+        MicroBitEvent evt(MICROBIT_ID_ACCELEROMETER, 0x1001);
+    }
+    else if ((accy < -500) && is_vertical && (screen_state != SCREEN_LOGO_DOWN)){
+        screen_state = SCREEN_LOGO_DOWN;
+        // send logo down event
+        MicroBitEvent evt(MICROBIT_ID_ACCELEROMETER, 0x1002);
+    }
+    else if ((accz < -500) && is_flat && (screen_state != SCREEN_SCREEN_UP)){
+        screen_state = SCREEN_SCREEN_UP;
+    }
+    else {
+        // other state, reset state engine
+        //screen_state = SCREEN_STATE_UNKNOWN;
+    }
 }
 
 void app_main() {
-    uBit.MessageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onButtonAClick);
-    uBit.MessageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, onButtonBClick);
+    uBit.MessageBus.listen(MES_REMOTE_CONTROL_ID, MICROBIT_EVT_ANY, onPlayPauseEvent);
     uBit.display.print('X');
 
     while (1) {
-       uBit.sleep(1000);
+       detect_screen();
+       uBit.sleep(50);
     }
 }
